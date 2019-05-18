6e7b0f57a23f5ce33fb02ec16b0717d9d3e9cdea
Marcelo Buratti de Freitas <marcelofreitas21@hotmail.com>
2019-05-18 16:38:26 -0300

    Update FirebaseConection.js

--- src/FirebaseConection.js
+++ src/FirebaseConection.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyAZTiUXPNqMX9T382zkXvOgKRQOGyIv-qE",
-    authDomain: "devapp-cc207.firebaseapp.com",
-    databaseURL: "https://devapp-cc207.firebaseio.com",
-    projectId: "devapp-cc207",
-    storageBucket: "devapp-cc207.appspot.com",
-    messagingSenderId: "379945487180"
+    apiKey: "xxxxxxx",
+    authDomain: "xxxxxxx",
+    databaseURL: "xxxxx",
+    projectId: "xxxxxx",
+    storageBucket: "xxxxx",
+    messagingSenderId: "xxxxxxxx"
   };
   firebase.initializeApp(config);
 
